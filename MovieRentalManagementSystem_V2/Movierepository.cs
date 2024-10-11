894f2107d3b81de529df343d9eefc639960f8271
UT01336 <UT01336@UNICOMTIC52>
2024-10-11 12:07:24 +0530

    Add project files.

---
+++ MovieRentalManagementSystem_V2/Movierepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem_V2
+{
+    public class Movierepository
+    {
+        private static _connectionstring("Server ")
+    }
+}
